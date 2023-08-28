315352f08b5cf013c11a41c2d316f7c1ed7cfe13
IP-C <138649184+IP-C@users.noreply.github.com>
2023-08-28 17:46:44 +0900

    修正02

--- app/controllers/user/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-##class Users::RegistrationsController < Devise::RegistrationsController
-  ### protected
-
-  ##def configure_sign_up_params
-    ##devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile, :occupation, :position])
-  #end
-#end
-
